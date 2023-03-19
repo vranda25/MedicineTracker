5208992425519a9df402394718fd46dbea15079f
Vranda Mahajan <202052349@iiitvadodara.ac.in>
2023-03-19 11:04:22 +0530

    new commit

--- medicineTracker/medicineTracker/settings.py
+++ medicineTracker/medicineTracker/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'account',
-    'home',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- medicineTracker/medicineTracker/urls.py
+++ medicineTracker/medicineTracker/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('account/', include('account.urls')),
-    path('/', include('home.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
